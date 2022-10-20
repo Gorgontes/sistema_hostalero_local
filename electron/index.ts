d18d5e80a3a45412a4890240d5fbfac034898012
Gorgontes <montesluisome@gmail.com>
2022-10-19 19:18:24 -0500

    first commit

---
+++ electron/index.ts
@@ -0,0 +1,69 @@
+// Native
+import { join } from 'path';
+
+// Packages
+import { BrowserWindow, app, ipcMain, IpcMainEvent } from 'electron';
+import isDev from 'electron-is-dev';
+
+const height = 600;
+const width = 800;
+
+function createWindow() {
+  const window = new BrowserWindow({
+    width,
+    height,
+    // frame: false,
+    show: true,
+    resizable: true,
+    fullscreenable: true,
+    webPreferences: {
+      preload: join(__dirname, 'preload.js')
+    }
+  });
+
+  const port = process.env.PORT || 3000;
+  const url = isDev ? `http://localhost:${port}` : join(__dirname, '../src/out/index.html');
+
+  if (isDev) {
+    window?.loadURL(url);
+  } else {
+    window?.loadFile(url);
+  }
+
+  // For AppBar
+  ipcMain.on('minimize', () => {
+    window.isMinimized() ? window.restore() : window.minimize();
+    // or alternatively: win.isVisible() ? win.hide() : win.show()
+  });
+  ipcMain.on('maximize', () => {
+    window.isMaximized() ? window.restore() : window.maximize();
+  });
+
+  ipcMain.on('close', () => {
+    window.close();
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+// listen the channel `message` and resend the received message to the renderer process
+ipcMain.on('message', (event: IpcMainEvent, message: any) => {
+  console.log(message);
+  setTimeout(() => event.sender.send('message', 'hi from electron'), 500);
+});
