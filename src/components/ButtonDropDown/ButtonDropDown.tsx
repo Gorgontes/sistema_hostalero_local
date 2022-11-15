e2437b2248a0761ddaca89bbfdc9c84cfa0860f9
kleyson klaus <kleyson.zur44@gmail.com>
2022-11-14 19:55:57 -0500

    feat(merge conflicts): fix merge

--- src/components/ButtonDropDown/ButtonDropDown.tsx
+++ src/components/ButtonDropDown/ButtonDropDown.tsx
@@ -1,53 +1,53 @@
 import cntl from "cntl";
 import React, { useState } from "react";
 import ItemDropButton from "../ItemDropDown/ItemDropDown";
-import "./style.css";
+
 type Props = {
-  label: string;
-  items: Array<{
     label: string;
-    url: string;
-  }>;
+    items: Array<{
+        label: string;
+        url: string;
+    }>;
 };
 
 const ButtonDropDown = (props: Props) => {
-  const [isHover, setIsHover] = useState(false);
+    const [isHover, setIsHover] = useState(false);
 
-  return (
-    <div className="dropdown relative inline-block">
-      <button
-        className="dropbtn hover:bg-background_main hover:text-primario text-background_main py-2 px-5 text-sm"
-        onMouseEnter={() => {
-          setIsHover(true);
-        }}
-        onMouseLeave={() => {
-          setIsHover(false);
-        }}
-      >
-        {props.label}
-      </button>
-      <div
-        onMouseEnter={() => {
-          setIsHover(true);
-        }}
-        onMouseLeave={() => {
-          setIsHover(false);
-        }}
-        className={cntl`
+    return (
+        <div className="dropdown relative inline-block">
+            <button
+                className="dropbtn hover:bg-background_main hover:text-primario text-background_main py-2 px-5 text-sm"
+                onMouseEnter={() => {
+                    setIsHover(true);
+                }}
+                onMouseLeave={() => {
+                    setIsHover(false);
+                }}
+            >
+                {props.label}
+            </button>
+            <div
+                onMouseEnter={() => {
+                    setIsHover(true);
+                }}
+                onMouseLeave={() => {
+                    setIsHover(false);
+                }}
+                className={cntl`
                     absolute z-10 min-w-[160px]
                     bg-background_main border-b-[5px]
                     border-primario rounded-b-lg
                     shadow-lg
                     text-primario ${isHover ? "block" : "hidden"}`}
-      >
-        {props.items.map((json, index) => {
-          return (
-            <ItemDropButton key={index} label={json.label} url={json.url} />
-          );
-        })}
-      </div>
-    </div>
-  );
+            >
+                {props.items.map((json, index) => {
+                    return (
+                        <ItemDropButton key={index} label={json.label} url={json.url} />
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default ButtonDropDown;
--- src/components/ItemDropDown/ItemDropDown.tsx
+++ src/components/ItemDropDown/ItemDropDown.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 type Props = {
-  label: string;
-  url: string;
+    label: string;
+    url: string;
 };
 
 const ItemDropButton = ({ label, url }: Props) => {
-  return (
-    <a
-      className="py-2 px-4 hover:bg-primario hover:text-background_main rounded-sm block"
-      href={url}
-    >
-      {" "} {label}
-    </a>
-  );
+    return (
+        <a
+            className="py-2 px-4 hover:bg-primario hover:text-background_main rounded-sm block"
+            href={url}
+        >
+            {" "} {label}
+        </a>
+    );
 };
 
 export default ItemDropButton;
