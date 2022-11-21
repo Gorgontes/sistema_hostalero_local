0ae46e488fdbd9cbc2b41dd46876caa37b85bd50
Gorgontes <montesluisome@gmail.com>
2022-11-21 02:18:51 -0500

    refactor algunas cosas en el formulario

--- src/components/Habitaciones/NewHabitacionForm.tsx
+++ src/components/Habitaciones/NewHabitacionForm.tsx
@@ -2,6 +2,7 @@ import {
   Button,
   Checkbox,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input,
   NumberInput,
@@ -10,94 +11,128 @@ import {
 } from "@chakra-ui/react";
 import { Prisma } from "@prisma/client";
 import { Field, FieldProps, Form, Formik } from "formik";
-import { useState } from "react";
-import * as yup from 'yup';
+import * as Yup from "yup";
 
 type Props = {
-  callback?: (habitacion: Omit<Prisma.HabitacionCreateInput, 'piso'>) => void;
+  callback?: (habitacion: Omit<Prisma.HabitacionCreateInput, "piso">) => void;
   initialValues?: InitialValues;
 };
 interface InitialValues {
-  nombreHabitacion: string,
-  precioReferencial: number | string,
-  tv: boolean,
-  wifi: boolean,
-  banos: number | string,
-  camas: number | string,
-  descripcion: string,
-  observaciones: string,
+  nombreHabitacion: string;
+  precioReferencial: number | string;
+  tv: boolean;
+  wifi: boolean;
+  banos: number | string;
+  camas: number | string;
+  descripcion: string;
+  observaciones: string;
 }
 const initialValuesDefault = {
   nombreHabitacion: "",
-  precioReferencial: 0,
+  precioReferencial: 110,
   tv: true,
   wifi: true,
-  banos: 1,
-  camas: 1,
+  banos: "1",
+  camas: "1",
   descripcion: "",
   observaciones: "",
 };
 
-const schema = yup.object().shape({
-  nombreHabitacion: yup.string().required(),
-  precioReferencial: yup.number().optional().positive(),
-  tv: yup.boolean().required(),
-  wifi: yup.boolean().required(),
-  banos: yup.number().integer().positive(),
-  camas: yup.number().integer().positive(),
-  descripcion: yup.string().optional(),
-  observaciones: yup.string().optional(),
-})
+const schema = Yup.object().shape({
+  nombreHabitacion: Yup.string().required(),
+  //este regex valida si es un numero con 2 digitos
+  precioReferencial: Yup.string().optional().matches(/^\s*-?(\d+(\.\d{1,2})?|\.\d{1,2})\s*$/),
+  tv: Yup.boolean(),
+  wifi: Yup.boolean(),
+  banos: Yup.string().required().matches(/^[1-9]\d*$/, 'No es un numero valido'),
+  camas: Yup.string().required().matches(/^[1-9]\d*$/, 'No es un numero valido'),
+  descripcion: Yup.string().optional(),
+  observaciones: Yup.string().optional(),
+});
 function validate(values: InitialValues) {
-  console.log('validando');
+  // console.log("validando");
   return {};
 }
 
 const HabitacionForm = (props: Props) => {
-  console.log('rendering');
+  // console.log("rendering");
+  // console.log("schema", schema);
+  // console.log("spec", (schema.fields["nombreHabitacion"] as any).spec);
+  schema.validate({
+    nombreHabitacion: "dd",
+    precioReferencial: 1,
+    tv: true,
+    wifi: true,
+    banos: "1",
+    camas: "1",
+    descripcion: "",
+    observaciones: "",
+  }).then((val) => console.log(val)).catch(e => console.warn(e));
   return (
     <Formik
-      initialValues={props.initialValues ? props.initialValues : initialValuesDefault}
-      validate={validate}
-      onSubmit={(values, {setSubmitting}) => {
-        // props.callback && props.callback(values);
-        setSubmitting(false);
+      initialValues={
+        props.initialValues ? props.initialValues : initialValuesDefault
+      }
+      validationSchema={schema}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log(values);
       }}
     >
       {({ isSubmitting }) => (
         <Form className="py-2 px-5 space-y-2">
-          <Field name="nombreHabitacion">
-            {({ field, form }: FieldProps) => (
-              <FormControl className="flex items-baseline">
+          <Field name="nombreHabitacion" isR>
+            {({ field, form, meta }: FieldProps) => (
+              <FormControl
+                className="grid grid-cols-[1fr,20rem] gap-x-4"
+                isRequired
+                isInvalid={!!meta.error}
+              >
                 <FormLabel className="whitespace-nowrap text-primario">
-                  Nombre y/o numero de habitacion
+                  Nombre y/o numero de habitacion 
                 </FormLabel>
                 <Input {...field} />
+                <FormErrorMessage className="col-span-2">
+                  Nombre y/o numero de habitacion es requerido
+                </FormErrorMessage>
               </FormControl>
             )}
           </Field>
+
           <div className="grid grid-cols-[minmax(0,4fr),minmax(0,2fr),minmax(0,4fr)] grid-rows-2 gap-2 grid-flow-col">
             <Field name="camas">
-              {({ field }: FieldProps) => (
-                <FormControl className="flex items-baseline justify-end">
+              {({ field: {value, onChange, name} , meta, form}: FieldProps) => (
+                <FormControl
+                  className="flex items-baseline justify-end"
+                  isRequired={!!meta.error}
+                >
                   <FormLabel className="text-primario">Camas</FormLabel>
-                  <Input {...field} className="!w-12 mx-4" />
+                  <NumberInput className="!w-20 mx-4" value={value} onChange={val => form.setFieldValue(name, val)}>
+                    <NumberInputField className="!pr-2"/>
+                  </NumberInput>
                 </FormControl>
               )}
             </Field>
             <Field name="banos">
-              {({ field }: FieldProps) => (
-                <FormControl className="flex items-baseline justify-end">
+              {({ field , meta, form }: FieldProps) => (
+                <FormControl
+                  className="flex items-baseline justify-end"
+                  isRequired={ !!meta.error }
+                  isInvalid={ !!meta.error }
+                >
                   <FormLabel className="text-primario">
                     Baños privados
                   </FormLabel>
-                  <Input {...field} className="!w-12 mx-4" />
+                  <NumberInput className="!w-20 mx-4" value={field.value} onChange={val => {form.setFieldValue(field.name, val)}} >
+                    <NumberInputField className="!pr-2"/>
+                  </NumberInput>
                 </FormControl>
               )}
             </Field>
             <Field name="tv" type="checkbox">
-              {({ field }: FieldProps) => (
-                <FormControl className="flex items-baseline justify-end pt-3">
+              {({ field, meta }: FieldProps) => (
+                <FormControl
+                  className="flex items-baseline justify-end pt-1"
+                >
                   <FormLabel className="text-primario">TV</FormLabel>
                   {/* <Checkbox {...field}/> */}
                   <Checkbox
@@ -109,8 +144,8 @@ const HabitacionForm = (props: Props) => {
               )}
             </Field>
             <Field name="wifi" type="checkbox">
-              {({ field }: FieldProps) => (
-                <FormControl className="flex items-baseline justify-end pt-3">
+              {({ field, meta }: FieldProps) => (
+                <FormControl className="flex items-baseline justify-end pt-1">
                   <FormLabel className="text-primario">WIFI</FormLabel>
                   <Checkbox
                     isChecked={field.checked}
@@ -121,36 +156,40 @@ const HabitacionForm = (props: Props) => {
               )}
             </Field>
             <Field name="precioReferencial">
-              {({ field }: FieldProps) => (
-                <FormControl className="row-span-2">
+              {({ field, meta }: FieldProps) => (
+                <FormControl
+                  className="row-span-2"
+                >
                   <FormLabel className="whitespace-nowrap text-primario !text-center">
                     Precio referencial
                   </FormLabel>
                   <div className="flex justify-center items-center">
                     <div className="mr-5 text-primario">S/.</div>
                     {/* <Input/> */}
-                    
-                    <NumberInput  className="!w-40">
-                      <NumberInputField {...field}/>
+
+                    <NumberInput className="!w-40">
+                      <NumberInputField {...field} />
                     </NumberInput>
                   </div>
+                  <FormErrorMessage>{meta.error}</FormErrorMessage>
                 </FormControl>
               )}
             </Field>
           </div>
           <Field name="descripcion">
-            {({ field }: FieldProps) => (
-              <FormControl>
+            {({ field, meta }: FieldProps) => (
+              <FormControl >
                 <FormLabel className="text-primario">Descripción</FormLabel>
                 <Textarea {...field} />
               </FormControl>
             )}
           </Field>
           <Field name="observaciones">
-            {({ field }: FieldProps) => (
-              <FormControl>
+            {({ field,meta }: FieldProps) => (
+              <FormControl >
                 <FormLabel className="text-primario">Observaciones</FormLabel>
                 <Textarea {...field} />
+                  <FormErrorMessage>{meta.error}</FormErrorMessage>
               </FormControl>
             )}
           </Field>
