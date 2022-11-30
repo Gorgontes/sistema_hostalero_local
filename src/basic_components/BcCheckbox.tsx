60247ba90f104ca07e588e57307e27eed7b687a6
kleyson klaus <kleyson.zur44@gmail.com>
2022-11-30 00:30:29 -0500

    feat(basic_components): not implemented yet

---
+++ src/basic_components/BcCheckbox.tsx
@@ -0,0 +1,30 @@
+import cntl from "cntl";
+import { StringifyOptions } from "querystring";
+type Estado = "ocupada" | "libre" | "reservada";
+
+type Props = {
+    estado: Estado;
+    nombre: string;
+    onClick?: (event?: React.MouseEvent<HTMLElement>) => void;
+    className?: string;
+};
+
+const styleEstado = {
+    libre: cntl`text-primario bg-white border-primario border-4`,
+    ocupada: cntl`text-white bg-primario border-primario border-4`,
+    reservada: cntl`text-white bg-morado`,
+};
+
+const BcCheckbox = ({ className, estado, onClick, nombre }: Props) => {
+    const estilosAdicionales = `${styleEstado[estado]} ${className}`
+    return (
+        <div
+            className={`text-3xl font-bold py-2 rounded-lg text-center ${estilosAdicionales}`}
+            onClick={onClick}>
+            {nombre}
+        </div>
+    );
+};
+
+
+export default BcCheckbox;
